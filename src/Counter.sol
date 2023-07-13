dd7663582c7f7f04a172efee845cf494755807f5
Ankita-info456 <ankitagupta.infograins@gmail.com>
2023-07-13 11:22:30 +0530

    save

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,49 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
 contract Counter {
-    uint256 public number;
+    uint256 public count;
+    address immutable _owner;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    constructor() {
+       // _owner = msg.sender;
+       _owner = msg.sender;
+
     }
 
-    function increment() public {
-        number++;
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "owner is not present");
+        _;
+    }
+
+    function get() public view returns(uint) {
+        return count;
+    }
+
+    function inc() public {
+        count += 1;
+    }
+
+    function dec() public  {
+        count -= 1;
+    }
+
+    function hello() public pure returns(string memory) {
+        return "hi there";
+    }
+
+    function returnLoop() public view  onlyOwner returns(uint) {
+        uint temp = 0;
+        for(uint i = 0; i <=100; i++) {
+            temp+=1;
+        }
+        return temp;
+    }
+
+    function returnSender() public payable returns (uint) {
+        
+        return msg.value;
     }
 }
---
+++ src/imports.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.17;
+
+import "solmate/tokens/ERC20.sol";
+
+contract token is ERC20 ("NAME", "SYMBOL", 18) {
+
+}
